3410ddf942eb7b01db572821fc20162fb5e3232c
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2005-08-21 14:01:26 +0000

    Started Vizrea branch from 0.7
    git-svn-id: svn://dev.exiv2.org/svn@611 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

---
+++ branches/vizrea/test/addmoddel.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+# Test driver to run the addmoddel sample program
+results="./tmp/addmoddel.out"
+good="./data/addmoddel.out"
+diffargs="--strip-trailing-cr"
+tmpfile=tmp/ttt
+touch $tmpfile
+diff -q $diffargs $tmpfile $tmpfile 2>/dev/null
+if [ $? -ne 0 ] ; then
+    diffargs=""
+fi
+(
+LD_LIBRARY_PATH=../../src:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+binpath="../../src"
+cp -f ./data/exiv2-empty.jpg ./tmp
+cd ./tmp
+$binpath/addmoddel exiv2-empty.jpg
+$binpath/exiv2 -pv exiv2-empty.jpg
+) > $results
+
+diff -q $diffargs $results $good
+rc=$?
+if [ $rc -eq 0 ] ; then
+    echo "All testcases passed."
+else
+    diff $diffargs $results $good
+fi
