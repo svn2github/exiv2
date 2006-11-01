45b89c9da4580a368e2f6e475e24c0734d6b4fea
cgilles <cgilles@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2006-11-01 15:16:23 +0000

    Exiv2 from gettext branch : add new i18n.h file with common macro. First string to translate into tags.cpp
    git-svn-id: svn://dev.exiv2.org/svn@910 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

---
+++ branches/gettext/src/i18n.h
@@ -0,0 +1,32 @@
+/****************************************************************************
+ * Copyright (C) 2004, 2005, 2006 Andreas Huggel <ahuggel@gmx.net>
+ *
+ * This program is part of the Exiv2 distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301 USA.
+ *****************************************************************************/
+
+#ifndef __I18N_H__
+#define __I18N_H__
+
+#include "../config/config.h"
+#include "gettext.h"
+
+#ifdef ENABLE_NLS
+#define _(String) (String)
+#define N_(String) (String)
+#endif
+
+#endif /* __I18N_H__ */
