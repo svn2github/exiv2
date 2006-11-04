10ea3225a7918baa7344d3e071052d12d38c7c50
cgilles <cgilles@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2006-11-04 13:28:16 +0000

    Exiv2 from gettext branch : using EXV_PACKAGE
    git-svn-id: svn://dev.exiv2.org/svn@924 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

--- branches/gettext/src/i18n.h
+++ branches/gettext/src/i18n.h
@@ -35,10 +35,10 @@
 
 #include "gettext.h"
 
-#ifdef ENABLE_NLS
+#ifdef EXV_ENABLE_NLS
 #  include <libintl.h>
 #  undef _
-#  define _(String) dgettext (PACKAGE, String)
+#  define _(String) dgettext (EXV_PACKAGE, String)
 #  ifdef gettext_noop
 #    define N_(String) gettext_noop (String)
 #  else
