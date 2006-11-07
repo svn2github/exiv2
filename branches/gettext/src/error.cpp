c9aac74a7ab5b87f114f4f25f773327ced77cffa
cgilles <cgilles@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2006-11-07 09:59:09 +0000

    Exiv2 from gettext branch : error.cpp : N_() instead _()
    git-svn-id: svn://dev.exiv2.org/svn@952 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

--- branches/gettext/src/error.cpp
+++ branches/gettext/src/error.cpp
@@ -41,48 +41,48 @@ EXIV2_RCSID("@(#) $Id$")
 namespace Exiv2 {
 
     const ErrMsg Error::errMsg_[] = {
-        ErrMsg( -1, _("Error %0: arg1=%1, arg2=%2, arg3=%3.")),
-        ErrMsg(  0, _("Success")),
+        ErrMsg( -1, N_("Error %0: arg1=%1, arg2=%2, arg3=%3.")),
+        ErrMsg(  0, N_("Success")),
         ErrMsg(  1, "%1"), // %1=error message
         ErrMsg(  2, "%1: %2 (%3)"), // %1=path, %2=strerror, %3=function that failed
-        ErrMsg(  3, _("This does not look like a %1 image")), // %1=Image type
-        ErrMsg(  4, _("Invalid dataset name `%1'")), // %1=dataset name
-        ErrMsg(  5, _("Invalid record name `%1'")), // %1=record name
-        ErrMsg(  6, _("Invalid key `%1'")), // %1=key
-        ErrMsg(  7, _("Invalid tag name or ifdId `%1', ifdId %2")), // %1=tag name, %2=ifdId
-        ErrMsg(  8, _("Value not set")),
-        ErrMsg(  9, _("%1: Failed to open the data source: %2")), // %1=path, %2=strerror
-        ErrMsg( 10, _("%1: Failed to open file (%2): %3")), // %1=path, %2=mode, %3=strerror
-        ErrMsg( 11, _("%1: The file contains data of an unknown image type")), // %1=path
-        ErrMsg( 12, _("The memory contains data of an unknown image type")),
-        ErrMsg( 13, _("Image type %1 is not supported")), // %1=image type
-        ErrMsg( 14, _("Failed to read image data")),
-        ErrMsg( 15, _("This does not look like a JPEG image")),
-        ErrMsg( 16, _("MakerTagInfo registry full")),
-        ErrMsg( 17, _("%1: Failed to rename file to %2: %3")), // %1=old path, %2=new path, %3=strerror
-        ErrMsg( 18, _("%1: Transfer failed: %2")), // %1=path, %2=strerror
-        ErrMsg( 19, _("Memory transfer failed: %1")), // %1=strerror
-        ErrMsg( 20, _("Failed to read input data")),
-        ErrMsg( 21, _("Failed to write image")),
-        ErrMsg( 22, _("Input data does not contain a valid image")),
-        ErrMsg( 23, _("Failed to create Makernote for ifdId %1")), // %1=ifdId
-        ErrMsg( 24, _("Entry::setValue: Value too large (tag=%1, size=%2, requested=%3)")), // %1=tag, %2=dataSize, %3=required size
-        ErrMsg( 25, _("Entry::setDataArea: Value too large (tag=%1, size=%2, requested=%3)")), // %1=tag, %2=dataAreaSize, %3=required size
-        ErrMsg( 26, _("Offset out of range")),
-        ErrMsg( 27, _("Unsupported data area offset type")),
-        ErrMsg( 28, _("Invalid charset: `%1'")), // %1=charset name
-        ErrMsg( 29, _("Unsupported date format")),
-        ErrMsg( 30, _("Unsupported time format")),
-        ErrMsg( 31, _("%1: CRW images don't support IPTC metadata")), // %1=function
-        ErrMsg( 32, _("%1: CRW images don't support JPEG comments")), // %1=function
-        ErrMsg( 33, _("This does not look like a CRW image")),
-        ErrMsg( 34, _("%1: Not supported")), // %1=function
-        ErrMsg( 35, _("ImageFactory registry full")),
-        ErrMsg( 36, _("Failed to decode %1 metadata")), // %1=type of metadata (Exif, IPTC)
-        ErrMsg( 37, _("Size of %1 JPEG segment is larger than 65535 bytes")), // %1=type of metadata (Exif, IPTC, JPEG comment)
+        ErrMsg(  3, N_("This does not look like a %1 image")), // %1=Image type
+        ErrMsg(  4, N_("Invalid dataset name `%1'")), // %1=dataset name
+        ErrMsg(  5, N_("Invalid record name `%1'")), // %1=record name
+        ErrMsg(  6, N_("Invalid key `%1'")), // %1=key
+        ErrMsg(  7, N_("Invalid tag name or ifdId `%1', ifdId %2")), // %1=tag name, %2=ifdId
+        ErrMsg(  8, N_("Value not set")),
+        ErrMsg(  9, N_("%1: Failed to open the data source: %2")), // %1=path, %2=strerror
+        ErrMsg( 10, N_("%1: Failed to open file (%2): %3")), // %1=path, %2=mode, %3=strerror
+        ErrMsg( 11, N_("%1: The file contains data of an unknown image type")), // %1=path
+        ErrMsg( 12, N_("The memory contains data of an unknown image type")),
+        ErrMsg( 13, N_("Image type %1 is not supported")), // %1=image type
+        ErrMsg( 14, N_("Failed to read image data")),
+        ErrMsg( 15, N_("This does not look like a JPEG image")),
+        ErrMsg( 16, N_("MakerTagInfo registry full")),
+        ErrMsg( 17, N_("%1: Failed to rename file to %2: %3")), // %1=old path, %2=new path, %3=strerror
+        ErrMsg( 18, N_("%1: Transfer failed: %2")), // %1=path, %2=strerror
+        ErrMsg( 19, N_("Memory transfer failed: %1")), // %1=strerror
+        ErrMsg( 20, N_("Failed to read input data")),
+        ErrMsg( 21, N_("Failed to write image")),
+        ErrMsg( 22, N_("Input data does not contain a valid image")),
+        ErrMsg( 23, N_("Failed to create Makernote for ifdId %1")), // %1=ifdId
+        ErrMsg( 24, N_("Entry::setValue: Value too large (tag=%1, size=%2, requested=%3)")), // %1=tag, %2=dataSize, %3=required size
+        ErrMsg( 25, N_("Entry::setDataArea: Value too large (tag=%1, size=%2, requested=%3)")), // %1=tag, %2=dataAreaSize, %3=required size
+        ErrMsg( 26, N_("Offset out of range")),
+        ErrMsg( 27, N_("Unsupported data area offset type")),
+        ErrMsg( 28, N_("Invalid charset: `%1'")), // %1=charset name
+        ErrMsg( 29, N_("Unsupported date format")),
+        ErrMsg( 30, N_("Unsupported time format")),
+        ErrMsg( 31, N_("%1: CRW images don't support IPTC metadata")), // %1=function
+        ErrMsg( 32, N_("%1: CRW images don't support JPEG comments")), // %1=function
+        ErrMsg( 33, N_("This does not look like a CRW image")),
+        ErrMsg( 34, N_("%1: Not supported")), // %1=function
+        ErrMsg( 35, N_("ImageFactory registry full")),
+        ErrMsg( 36, N_("Failed to decode %1 metadata")), // %1=type of metadata (Exif, IPTC)
+        ErrMsg( 37, N_("Size of %1 JPEG segment is larger than 65535 bytes")), // %1=type of metadata (Exif, IPTC, JPEG comment)
 
         // Last error message (message is not used)
-        ErrMsg( -2, _("(Unknown Error)"))
+        ErrMsg( -2, N_("(Unknown Error)"))
     };
 
     int Error::errorIdx(int code)
