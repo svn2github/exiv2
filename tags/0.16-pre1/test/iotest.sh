33dcfb60f473ebad64469b321cd2b3cef6f3dc94
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2007-11-04 13:38:22 +0000

    Tagged release 0.16-pre1
    git-svn-id: svn://dev.exiv2.org/svn@1321 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

---
+++ tags/0.16-pre1/test/iotest.sh
@@ -0,0 +1,58 @@
+#! /bin/sh
+# Test driver for image file i/o
+
+ioTest()
+{
+    src=$datapath/$1
+    out1=${1}.1
+    out2=${1}.2
+
+    #run tests
+    $binpath/iotest $src $out1 $out2
+    if [ $? -ne 0 ]; then
+       errors=`expr $errors + 1`
+       return
+    fi 
+
+    #check results
+    diffCheck $out1 $src 
+    diffCheck $out2 $src 
+    echo -n "."
+}
+
+# Make sure to pass the test file first and the known good file second
+diffCheck() 
+{
+    test=$1
+    good=$2
+    
+    #run diff and check results
+    diff -q --binary $test $good
+    if [ $? -ne 0 ]; then
+       errors=`expr $errors + 1`
+    else
+       rm $test
+    fi 
+}
+
+# **********************************************************************
+# main
+
+binpath="$VALGRIND ../../samples"
+datapath="../data"
+
+test_files="table.jpg smiley2.jpg ext.dat"
+
+errors=0
+cd ./tmp
+echo
+
+echo -n "Io tests"
+for i in $test_files; do ioTest $i; done
+
+echo -e "\n---------------------------------------------------------"
+if [ $errors -eq 0 ]; then
+   echo 'All test cases passed'
+else
+   echo $errors 'test case(s) failed!'
+fi
