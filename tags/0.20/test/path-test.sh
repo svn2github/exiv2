2692e2b7a465132cb918b79845001de086110f48
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2010-05-29 13:42:56 +0000

    Removed tag 0.20
    git-svn-id: svn://dev.exiv2.org/svn@2251 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

--- tags/0.20/test/path-test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# Mini test-driver for path utility functions
-LD_LIBRARY_PATH=../../src:$LD_LIBRARY_PATH
-binpath="$VALGRIND ../../src"
-cd ./tmp
-
-$binpath/path-test ../data/path-test.txt
