57bb56466727006ec7bef19b1bef5c0fe8705cd5
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2013-09-09 23:18:19 +0000

    samples/exifprint.cpp: syntax changed.
    git-svn-id: svn://dev.exiv2.org/svn@3157 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

--- branches/gsoc13/samples/exifprint.cpp
+++ branches/gsoc13/samples/exifprint.cpp
@@ -13,51 +13,53 @@ int main(int argc, char* const argv[])
 try {
 
     if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " file [{--nocurl | --curl} {--struc}]+\n";
+        std::cout << "Usage: " << argv[0] << "[[ {--nocurl | --curl} {--struc}]+] file]+\n";
         return 1;
     }
 
     bool useCurlFromExiv2TestApps = true;
-    bool printStructure = false;
+    bool printStructure           = false;
     for ( int a = 1 ; a < argc ; a++ ) {
         std::string arg(argv[a]);
         if      (arg == "--nocurl") useCurlFromExiv2TestApps = false;
         else if (arg == "--curl"  ) useCurlFromExiv2TestApps = true;
         else if (arg == "--struc" ) printStructure = true;
+        else {
+			Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(arg, useCurlFromExiv2TestApps);
+			assert(image.get() != 0);
+
+			if (printStructure) {
+				// print the structure of image file.
+				image->printStructure();
+			} else {
+				// print the metadata
+				image->readMetadata();
+
+				Exiv2::ExifData &exifData = image->exifData();
+				if (exifData.empty()) {
+					std::string error(arg);
+					error += ": No Exif data found in the file";
+					std::cerr << error << std::endl;
+				}
+				Exiv2::ExifData::const_iterator end = exifData.end();
+				for (Exiv2::ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
+					const char* tn = i->typeName();
+					std::cout << std::setw(44) << std::setfill(' ') << std::left
+							  << i->key() << " "
+							  << "0x" << std::setw(4) << std::setfill('0') << std::right
+							  << std::hex << i->tag() << " "
+							  << std::setw(9) << std::setfill(' ') << std::left
+							  << (tn ? tn : "Unknown") << " "
+							  << std::dec << std::setw(3)
+							  << std::setfill(' ') << std::right
+							  << i->count() << "  "
+							  << std::dec << i->value()
+							  << "\n";
+				}
+			}
+		}
     }
 
-    Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(argv[1], useCurlFromExiv2TestApps);
-    assert(image.get() != 0);
-
-    if (printStructure) {
-    	// print the structure of image file.
-        image->printStructure();
-    } else {
-		// print the metadata
-		image->readMetadata();
-
-		Exiv2::ExifData &exifData = image->exifData();
-		if (exifData.empty()) {
-			std::string error(argv[1]);
-			error += ": No Exif data found in the file";
-			throw Exiv2::Error(1, error);
-		}
-		Exiv2::ExifData::const_iterator end = exifData.end();
-		for (Exiv2::ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
-			const char* tn = i->typeName();
-			std::cout << std::setw(44) << std::setfill(' ') << std::left
-					  << i->key() << " "
-					  << "0x" << std::setw(4) << std::setfill('0') << std::right
-					  << std::hex << i->tag() << " "
-					  << std::setw(9) << std::setfill(' ') << std::left
-					  << (tn ? tn : "Unknown") << " "
-					  << std::dec << std::setw(3)
-					  << std::setfill(' ') << std::right
-					  << i->count() << "  "
-					  << std::dec << i->value()
-					  << "\n";
-		}
-    }
 
     return 0;
 }
