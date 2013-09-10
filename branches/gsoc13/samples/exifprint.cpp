10f8748a661b7c6e7045e3bbecb6d86ce045bd27
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2013-09-10 08:20:43 +0000

    samples/exifprint: better command-line parser
    git-svn-id: svn://dev.exiv2.org/svn@3158 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

--- branches/gsoc13/samples/exifprint.cpp
+++ branches/gsoc13/samples/exifprint.cpp
@@ -8,60 +8,76 @@
 #include <iomanip>
 #include <cassert>
 #include <cstring>
+#include <vector>
+#include <string>
 
 int main(int argc, char* const argv[])
 try {
+    int result = 0 ;
 
-    if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << "[[ {--nocurl | --curl} {--struc}]+] file]+\n";
-        return 1;
-    }
-
+    // parse command-line
     bool useCurlFromExiv2TestApps = true;
     bool printStructure           = false;
+    std::vector<std::string> files;
+        
     for ( int a = 1 ; a < argc ; a++ ) {
-        std::string arg(argv[a]);
-        if      (arg == "--nocurl") useCurlFromExiv2TestApps = false;
-        else if (arg == "--curl"  ) useCurlFromExiv2TestApps = true;
-        else if (arg == "--struc" ) printStructure = true;
-        else {
-			Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(arg, useCurlFromExiv2TestApps);
-			assert(image.get() != 0);
+        char* arg  = argv[a];
+        bool  bOpt = *arg == '-';
+        // skip past the -'s on the argument
+        while ( *arg == '-' ) arg++;
 
-			if (printStructure) {
-				// print the structure of image file.
-				image->printStructure();
-			} else {
-				// print the metadata
-				image->readMetadata();
+        if ( bOpt ) {
+            std::string opt(arg);
+            if      (opt == "nocurl") useCurlFromExiv2TestApps = false;
+            else if (opt == "curl"  ) useCurlFromExiv2TestApps = true;
+            else if (opt == "struc" ) printStructure = true;
+        } else {
+            files.push_back(arg);
+        }
+    }
+    
+    if ( !files.size() ) {
+        std::cout << "Usage: " << argv[0] << " [[ {--nocurl | --curl} {--struc}]+] file ]+\n";
+        result = 1;
+    }
+    
+    // process files in sequences
+    for ( size_t f = 0 ; f < files.size() ; f++ ) {
+        std::string&          file  = files[f];
+        Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(file, useCurlFromExiv2TestApps);
+        assert(image.get() != 0);
 
-				Exiv2::ExifData &exifData = image->exifData();
-				if (exifData.empty()) {
-					std::string error(arg);
-					error += ": No Exif data found in the file";
-					std::cerr << error << std::endl;
-				}
-				Exiv2::ExifData::const_iterator end = exifData.end();
-				for (Exiv2::ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
-					const char* tn = i->typeName();
-					std::cout << std::setw(44) << std::setfill(' ') << std::left
-							  << i->key() << " "
-							  << "0x" << std::setw(4) << std::setfill('0') << std::right
-							  << std::hex << i->tag() << " "
-							  << std::setw(9) << std::setfill(' ') << std::left
-							  << (tn ? tn : "Unknown") << " "
-							  << std::dec << std::setw(3)
-							  << std::setfill(' ') << std::right
-							  << i->count() << "  "
-							  << std::dec << i->value()
-							  << "\n";
-				}
-			}
-		}
+        if (printStructure) {
+            // print the structure of image file.
+            image->printStructure();
+        } else {
+            // print the metadata
+            image->readMetadata();
+
+            Exiv2::ExifData &exifData = image->exifData();
+            if (exifData.empty()) {
+                std::string error = file + ": No Exif data found in the file";
+                std::cerr << error << std::endl;
+            }
+            Exiv2::ExifData::const_iterator end = exifData.end();
+            for (Exiv2::ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
+                const char* tn = i->typeName();
+                std::cout << std::setw(44) << std::setfill(' ') << std::left
+                          << i->key() << " "
+                          << "0x" << std::setw(4) << std::setfill('0') << std::right
+                          << std::hex << i->tag() << " "
+                          << std::setw(9) << std::setfill(' ') << std::left
+                          << (tn ? tn : "Unknown") << " "
+                          << std::dec << std::setw(3)
+                          << std::setfill(' ') << std::right
+                          << i->count() << "  "
+                          << std::dec << i->value()
+                          << "\n";
+            }
+        }
     }
 
-
-    return 0;
+    return result;
 }
 //catch (std::exception& e) {
 //catch (Exiv2::AnyError& e) {
