ad50590309cbb07c6fca2c6adf735292459e8591
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2014-08-27 04:24:40 +0000

    jenkins testing
    git-svn-id: svn://dev.exiv2.org/svn@3306 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

--- branches/gsoc13/jenkins_build.sh
+++ branches/gsoc13/jenkins_build.sh
@@ -1,23 +1,59 @@
 #!/bin/bash
 
-set +v
-DIR="$PWD"
-if [ -e /home/rmills/bin/.profile ]; then
-	source /home/rmills/bin/.profile
-fi
-if [ -e /Users/rmills/bin/.profile ]; then
-	source /Users/rmills/bin/.profile
-fi
-cd "$DIR"
+##
+# jenkins_build.sh
+#   called by jenkins to build/test exiv2 
+#   - jenkins sets environment variables)
+#   called from terminal 
+#   - script has build-in defaults for some environment variable)
+#
+##
 
-echo ----------------------
-export
-echo ----------------------
+##
+# functions
+run_tests() {
+	if [ "$tests" == true ]; then
+		make tests
+	fi
+}
 
-if [ -z "$tests" ]; then tests=true; fi
+##
+# Quick dodge, use rmills ~/bin/.profile to set some environment variables
+# This code will be removed later
+# set +v
+# DIR="$PWD"
+# if [ -e /home/rmills/bin/.profile ]; then
+#	source /home/rmills/bin/.profile
+# fi
+# if [ -e /Users/rmills/bin/.profile ]; then
+#	source /Users/rmills/bin/.profile
+# fi
+# cd "$DIR"
+
+export PLATFORM=''
+if [ `uname` == Darwin  ]; then
+    PLATFORM=macosx
+elif [ `uname -o` == Cygwin ]; then
+    PLATFORM=cygwin
+elif [ `uname -o` == Msys ]; then
+    PLATFORM=mingw
+else
+    PLATFORM=linux
+fi
+
+##
+# set up some defaults (used when running this script from the terminal)
+if [                          -z "$tests"  ]; then export tests=true; fi
+if [ $PLATFORM == "macosx" -a -z "$macosx" ]; then macosx=true      ; fi
+if [ $PLATFORM == "cygwin" -a -z "cygwin"  ]; then cygwin=true      ; fi
+if [ $PLATFORM == "linux"  -a -z "$linux"  ]; then linux=true       ; fi
 
 export PATH=$PATH:/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/bin:/usr/lib/pkgconfig:/opt/local/bin:$PWD/usr/bin:/opt/local/bin:/opt/local/sbin:/opt/pkgconfig:bin
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PWD/usr/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
+
+echo   ----------------------
+export
+echo   ----------------------
 
 ##
 # cleanup from last time (if there was a last time)
@@ -31,7 +67,7 @@ if [ -e config/config.mk ]; then
     make distclean
 fi
 
-make config # 2>/dev/null >/dev/null
+make config # &>/dev/null
 
 ##
 # decide what to do about libcurl and libssh
@@ -68,9 +104,7 @@ if [ "$PLATFORM" == "cygwin" ]; then
 		make
 		make install
 		make samples
-		if [ "$tests" == true ]; then
-			make tests
-		fi
+		run_tests
 	fi
 fi
 
@@ -86,9 +120,7 @@ if [ $build == 1 ]; then
 	make "LDFLAGS=-L${PWD}/usr/lib -L${PWD}/xmpsdk/src/.libs"
 	make install
     make samples "CXXFLAGS=-I${PWD}/usr/include -I${PWD}/src" "LDFLAGS=-L${PWD}/usr/lib -L${PWD}/xmpsdk/src/.libs -lexiv2"
-	if [ "$tests" == true ]; then
-		make tests
-	fi
+    run_tests
 fi
 
 # That's all Folks!
