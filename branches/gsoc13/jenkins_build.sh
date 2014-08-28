224b546a4cc1bb201a53fb44d776a1995d50bb90
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2014-08-28 11:02:55 +0000

    jenkins development: debugging target/build variable
    git-svn-id: svn://dev.exiv2.org/svn@3324 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

--- branches/gsoc13/jenkins_build.sh
+++ branches/gsoc13/jenkins_build.sh
@@ -51,15 +51,15 @@ fi
 ##
 # set up some defaults (used when running this script from the terminal)
 echo "1 target = $target platform = $PLATFORM WORKSPACE = $WORKSPACE"
-if [ $PLATFORM == "macosx" -a -z "$macosx" ]; then export macosx=true ; export target=macosx ; fi
-if [ $PLATFORM == "cygwin" -a -z "$cygwin" ]; then export cygwin=true ; export target=cygwin ; fi
-if [ $PLATFORM == "linux"  -a -z "$linux"  ]; then export linux=true  ; export target=linux	 ; fi
-if [ $PLATFORM == "mingw"  -a -z "$mingw"  ]; then export mingw=true  ; export target=mingw	 ; fi
+if [ $PLATFORM == "macosx" -a -z "$macosx" ]; then export macosx=true ; export target=macosx    ; fi
+if [ $PLATFORM == "cygwin" -a -z "$cygwin" ]; then export cygwin=true ; export target=cygwin    ; fi
+if [ $PLATFORM == "linux"  -a -z "$linux"  ]; then export linux=true  ; export target=linux	    ; fi
+if [ $PLATFORM == "mingw"  -a -z "$mingw"  ]; then export mingw=true  ; export target=mingw	    ; fi
 
-if [ -z "$tests"     ]; then export tests=true                                               ; fi
-if [ -z "$WORKSPACE" ]; then export "WORKSPACE=$0/$PLATFORM"                                 ; fi
+if [ -z "$tests"     ]; then export tests=true                                                  ; fi
+if [ -z "$WORKSPACE" ]; then export WORKSPACE="$0/$PLATFORM"                                    ; fi
 
-if [ -z "$target" ]; then export target=$(basename $(echo $WORKSPACE | sed -e 's#\\#/#g'))   ; fi
+if [ -z "$target" ]; then export target=$(basename $(echo $WORKSPACE | sed -E -e 's#\\#/#g'))   ; fi
 echo "2 target = $target platform = $PLATFORM WORKSPACE = $WORKSPACE"
 
 export PATH=$PATH:/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/bin:/usr/lib/pkgconfig:/opt/local/bin:$PWD/usr/bin:/opt/local/bin:/opt/local/sbin:/opt/pkgconfig:bin
@@ -84,7 +84,7 @@ fi
 ##
 # create ./configure
 # we need to inspect configure to know if libssh and libcurl are options for this build
-make config # &>/dev/null
+make config &>/dev/null
 
 ##
 # decide what to do about curl and ssh
@@ -117,7 +117,7 @@ if [ $PLATFORM == "cygwin" -a "$target" == "cygwin" -a "$cygwin" == "true"  ]; t
 if [ $PLATFORM == "cygwin" -a "$target" == "mingw"  -a "$mingw"	 == "true"  ]; then build=$MING ; fi
 if [ $PLATFORM == "cygwin" -a "$target" == "msvc"   -a "$msvc"	 == "true"  ]; then build=$MSVC ; fi
 
-echo "2 target = $target platform = $PLATFORM WORKSPACE = $WORKSPACE build = $build = ${builds[$build]}"
+echo "3 target = $target platform = $PLATFORM WORKSPACE = $WORKSPACE build = $build = ${builds[$build]}"
 exit 0
 
 case "$build" in
