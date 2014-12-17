1c68a815a0f0a8e50550772fcf1b8e7ee6090a5f
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2014-12-17 19:09:32 +0000

    webready: msvc support for options openssl and libssh
    git-svn-id: svn://dev.exiv2.org/svn@3445 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

--- branches/webready/jenkins_build.bat
+++ branches/webready/jenkins_build.bat
@@ -26,6 +26,7 @@ if NOT DEFINED zlib          set zlib=true
 if NOT DEFINED curl          set curl=false
 if NOT DEFINED libssh        set libssh=false
 if NOT DEFINED openssl       set openssl=false
+if %openssl%==true           set curl=true
 
 if %ACTION%==/clean          set tests=false
 if %ACTION%==/upgrade        set tests=false
@@ -44,7 +45,7 @@ rem Pull in support libraries
 if NOT EXIST ..\expat    xcopy/yesihq  c:\exiv2libs\expat-2.1.0     ..\expat
 if NOT EXIST ..\zlib     xcopy/yesihq  c:\exiv2libs\zlib-1.2.7      ..\zlib
 if NOT EXIST ..\openssl  xcopy/yesihq  c:\exiv2libs\openssl-1.0.1j  ..\openssl
-if NOT EXIST ..\ssh      xcopy/yesihq  c:\exiv2libs\libssh-0.5.5    ..\ssh
+if NOT EXIST ..\libssh   xcopy/yesihq  c:\exiv2libs\libssh-0.5.5    ..\libssh
 if NOT EXIST ..\curl     xcopy/yesihq  c:\exiv2libs\curl-7.39.0     ..\curl
 
 rem --
@@ -52,12 +53,39 @@ rem build and test
 
 pushd msvc2005
 
+rem --
+rem FOO is the current directory in cygwin (/cygdrive/c/users/shared/workspace/exiv2-trunk/label/msvc)
+rem we need this to set the correct directory when we run the test suite from Cygwin
 for /f "tokens=*" %%a in ('cygpath -au ..') do set FOO=%%a
 
+rem --
+rem remove lines from the solution file to respect build options
+copy exiv2.sln e.sln
+rem find the lines which specify openssl, eay, ssh and curl
+for /f "tokens=*" %%a in ('findstr /c:openssl e.sln') do set L_OPENSSL=%%a
+for /f "tokens=*" %%a in ('findstr /c:libeay  e.sln') do set L_LIBEAY=%%a
+for /f "tokens=*" %%a in ('findstr /c:ssleay  e.sln') do set L_SSLEAY=%%a
+for /f "tokens=*" %%a in ('findstr /c:libssh  e.sln') do set L_LIBSSH=%%a
+for /f "tokens=*" %%a in ('findstr /c:curl    e.sln') do set L_CURL=%%a
+
+if %openssl%==false (
+    call BatchSubstitute.bat "%%L_OPENSSL%%" "" e.sln > d.sln && copy/y d.sln e.sln
+    call BatchSubstitute.bat "%%L_LIBEAY%%"  "" e.sln > d.sln && copy/y d.sln e.sln
+    call BatchSubstitute.bat "%%L_SSLEAY%%"  "" e.sln > d.sln && copy/y d.sln e.sln
+)
+if %libssh%==false (
+    call BatchSubstitute.bat "%%L_OPENSSL%%" "" e.sln > d.sln && copy d.sln/y e.sln
+)
+if %curl%==false (
+    call BatchSubstitute.bat "%%L_CURL%%"    "" e.sln > d.sln && copy/y d.sln e.sln
+)
+
+rem --
+rem Now build
 if %Win32%==true (
   if %debug%==true (
     if %static%==true (
-      devenv exiv2.sln %ACTION% "Debug|Win32"     
+      devenv e.sln %ACTION% "Debug|Win32"     
       if %tests%==true (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/Win32/Debug'
 ) ) ) )
@@ -65,7 +93,7 @@ if %Win32%==true (
 if %Win32%==true (
   if %release%==true (
     if %static%==true  (
-      devenv exiv2.sln %ACTION% "Release|Win32"    
+      devenv e.sln %ACTION% "Release|Win32"    
       if %tests%==true   (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/Win32/Release'
 ) ) ) )
@@ -73,7 +101,7 @@ if %Win32%==true (
 if %Win32%==true (
   if %debug%==true (
     if %dll%==true   (
-      devenv exiv2.sln %ACTION% "DebugDLL|Win32"   
+      devenv e.sln %ACTION% "DebugDLL|Win32"   
       if %tests%==true (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/Win32/DebugDLL'
 ) ) ) )
@@ -81,7 +109,7 @@ if %Win32%==true (
 if %Win32%==true (
   if %release%==true (
     if %dll%==true     (
-      devenv exiv2.sln %ACTION% "ReleaseDLL|Win32" 
+      devenv e.sln %ACTION% "ReleaseDLL|Win32" 
       if %tests%==true   (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/Win32/ReleaseDLL'
 ) ) ) )
@@ -89,7 +117,7 @@ if %Win32%==true (
 if %x64%==true (
   if %debug%==true (
     if %static%==true (
-      devenv exiv2.sln %ACTION% "Debug|x64"        
+      devenv e.sln %ACTION% "Debug|x64"        
       if %tests%==true (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/x64/Debug'
 ) ) ) )
@@ -97,7 +125,7 @@ if %x64%==true (
 if %x64%==true (
   if %release%==true (
     if %static%==true  (
-      devenv exiv2.sln %ACTION% "Release|x64"      
+      devenv e.sln %ACTION% "Release|x64"      
       if %tests%==true   (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/x64/Release'
 ) ) ) )
@@ -105,7 +133,7 @@ if %x64%==true (
 if %x64%==true (
   if %debug%==true (
     if %dll%==true   (
-      devenv exiv2.sln %ACTION% "DebugDLL|x64"     
+      devenv e.sln %ACTION% "DebugDLL|x64"     
       if %tests%==true (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/x64/DebugDLL'
 ) ) ) )
@@ -113,10 +141,13 @@ if %x64%==true (
 if %x64%==true   (
   if %release%==true (
     if %dll%==true     (
-      devenv exiv2.sln %ACTION% "ReleaseDLL|x64"   
+      devenv e.sln %ACTION% "ReleaseDLL|x64"   
       if %tests%==true   (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/x64/ReleaseDLL'
 ) ) ) )
+
+del e.sln
+del d.sln
 
 popd
 
