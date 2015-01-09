55ea24f39d5a7a7935d4448040e322ab9cd0f77e
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2015-01-09 11:53:27 +0000

    Copied trunk to branches/jenkins.
    git-svn-id: svn://dev.exiv2.org/svn@3549 b7c8b350-86e7-0310-a4b4-de8f6a8f16a3

---
+++ branches/jenkins/include/exiv2/http.hpp
@@ -0,0 +1,20 @@
+#ifndef HTTP_HPP_
+#define HTTP_HPP_
+
+#include <string>
+#include <map>
+#include <algorithm>
+
+namespace Exiv2 {
+    typedef std::map<std::string,std::string> dict_t;
+    typedef dict_t::iterator                  dict_i;
+
+    EXIV2API int http(dict_t& request,dict_t& response,std::string& errors);
+
+}
+
+#if EXV_USE_CURL
+#include <curl/curl.h>
+#endif
+
+#endif
